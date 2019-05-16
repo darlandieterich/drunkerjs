330823ce4e7a98277bf34ffdea4c2decdd2619e1
darlandieterich <darlandieterich@gmail.com>
2019-05-16 15:52:14 -0300

    Added squeleton effect runnaway and comments

--- src/drunker.js
+++ src/drunker.js
@@ -2,15 +2,22 @@ const Drunker = class Drunk {
 	constructor(element) {
 		this.element = element;
 	}
-
+	// Blurred effect
 	Blur() {
 	}
+	// Dizzy effect
 	Dizzy() {
 	}
+	// Disappear the element
 	Disappear() {
 	}
+	// Effect of fall element
 	Fall() {
 	}
+	// Effect runnaway the element
+	Runaway() {
+	}
+	// Effect random
 	Auto() {
 	}
 };
