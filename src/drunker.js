cf0ca6373fcbf257f99c3e4dd4bed75a8d9f03b7
darlandieterich <darlandieterich@gmail.com>
2019-05-19 21:33:15 -0300

    Added test module, enumerator of types

--- src/drunker.js
+++ src/drunker.js
@@ -1,6 +1,18 @@
 var Drunker = class Drunk extends Effects {
 	constructor(selector, effect) {
-		super(selectElement(selector), effect)	
+		super(selectElement(selector), effect);
+		this.types = TypeEffects("Blur", "Dizzy", "Disappear", "Shake");		
+	}
+
+	TypeEffects () {
+		for( var i = 0; i < arguments.length; ++i ){
+			this[arguments[i]] = i;
+		}
+		return this;
+	}
+
+	static GetTypes () {
+		return this.types;
 	}
 };
 
