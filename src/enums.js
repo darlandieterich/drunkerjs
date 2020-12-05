82ec6706a504d8aa6aa92046b5e055af76e4205a
darlandieterich <darlandieterich@gmail.com>
2020-12-04 23:33:05 -0300

    Added specific file enum to project

---
+++ src/enums.js
@@ -0,0 +1,23 @@
+//Type effects
+export const DrunkerEffects = {
+	BLUR: "Blur",
+	DIZZY: "Dizzy",
+	DISAPPEAR: "Disappear",
+	SHAKE: "Shake",
+	SPIN: "Spin",
+	PULSE: "Pulse",
+	AUTO: "Auto"
+};
+
+//Define velocity of effect
+export const DrunkerSpeed = {
+	SLOW: "Slow",
+	NORMAL: "Normal",
+	FAST: "Fast"
+}
+
+//Define the type of Drunker
+export const DrunkerType = {
+	MODERATE: "Moderate",  //One time
+	UNTILDROP: "UntilDrop" //Recursive mode
+}
